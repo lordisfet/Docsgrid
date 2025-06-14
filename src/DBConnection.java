8c3967a133ff59f967032c065c028e8ce59e3a4e
lordisfet <143896699+lordisfet@users.noreply.github.com>
2025-06-14 05:10:21 +0300

    add db connection

---
+++ src/DBConnection.java
@@ -0,0 +1,18 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBConnection {
+    private static final String URL = "jdbc:postgresql://localhost:5432/docsgrid";
+    private static final String USER = "postgres";
+    private static final String PASSWORD = "1212";
+
+    public static Connection connect() {
+        try {
+            return DriverManager.getConnection(URL, USER, PASSWORD);
+        } catch (SQLException e) {
+            System.err.println("Connection failed: " + e.getMessage());
+            return null;
+        }
+    }
+}
